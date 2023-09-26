c383eac2e3f37bc888ee0a52e638226ab0751722
kenneth <kennethdeleon7@gmail.com>
2023-09-26 11:57:25 +0800

    change theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+import colors from "tailwindcss/colors";
+import forms from "@tailwindcss/forms";
+import typography from "@tailwindcss/typography";
+
+export default {
+    content: ["./resources/**/*.blade.php", "./vendor/filament/**/*.blade.php"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                danger: colors.rose,
+                primary: {
+                    50: "#F0FFFC",
+                    100: "#D6FFF8",
+                    200: "#803333",
+                    300: "#803333",
+                    400: "#803333",
+                    500: "#801A1A",
+                    600: "#803333",
+                    700: "#803333",
+                    800: "#803333",
+                    900: "#803333",
+                },
+                success: colors.green,
+                warning: colors.yellow,
+            },
+        },
+    },
+    plugins: [forms, typography],
+};
